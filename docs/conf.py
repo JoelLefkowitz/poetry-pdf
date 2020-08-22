8ba9f5fb5bc6a3701dfaf45038590d9ba773eb9e
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-08-22 18:21:59 +0100

    Bump version: 1.0.2 → 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ path_guard("../..")
 
 
 project = "poetry-pdf"
-version = "1.0.2"
+version = "1.1.0"
 master_doc = "index"
 author = "Joel Lefkowitz"
 copyright = f"{datetime.datetime.now().year}, {author}"
